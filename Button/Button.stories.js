d8709b34e96b37900c9aea964c6ca0386256ea74
unknown <chigrus@yandex.ru>
2022-08-16 11:25:16 +0300

    modify button

--- Button/Button.stories.js
+++ Button/Button.stories.js
@@ -4,7 +4,7 @@ import PgButton from './Button.vue';
 export default {
   title: 'Атомы/Кнопка',
   component: PgButton,
-  // More on argTypes: https://storybook.js.org/docs/vue/api/argtypes111
+  // More on argTypes: https://storybook.js.org/docs/vue/api/argtypes
   argTypes: {
     backgroundColor: { control: 'color' },
     onClick: {},
